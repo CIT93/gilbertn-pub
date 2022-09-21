ad1d525a8f7b683961dbbb1589c858b16d6e82a1
Gilbert Negrillo <bertnegrillo@gmail.com>
2022-09-21 14:49:36 -0700

    Week 7 Learn Together Code Update

--- w7/app.js
+++ w7/app.js
@@ -1,3 +1,60 @@
+// Created an array with strings to show scheudle
+const schedule = [
+  'Monday, Biology from 11:00 am to 12:50 pm',
+  'Tuesday and Thursday, Health from 10:30 am to 11:45 am',
+  'Wednesday and Friday, no class (all day)',
+];
+
+// Created an array with objects of my classes. Total of 3 objects, every single one has what my class is (whatIsMyClass), when, and time
+const classes = [
+  {
+    whatIsMyClass: 'Biology',
+    when: 'Monday',
+    time: '11:00 am to 12:50 pm',
+  },
+  {
+    whatIsMyClass: 'Health',
+    when: 'Tuesday and Thursday',
+    time: '10:30 am to 11:45 am',
+  },
+  {
+    whatIsMyClass: 'No class',
+    when: 'Wednesday and Friday',
+    time: 'all day',
+  },
+];
+
+// Created a function that tells me my class schedule and what class I have to attend
+const whatIsMyClass = function () {
+  let classSchedule = Math.floor(Math.random() * 1100);
+  let whatIsMyClass = '';
+  if ((classSchedule = 'Monday' && classSchedule === 1100)) {
+    whatIsMyClass = 'Biology';
+    // Monday Biology class start time 11:00 am
+  } else if (
+    (classSchedule = 'Tuesday and Thursday' && classSchedule <= 1100)
+  ) {
+    whatIsMyClass = 'Health';
+    // Tuesday and Thursday Health class start time 10:30 am
+  } else {
+    whatIsMyClass = 'No class'; // Wednesday and Friday no classes
+  }
+  return whatIsMyClass;
+};
+
+// for each loop that tells me my decision
+const myDecision = function (whatIsMyClass) {
+  let myDecision = '';
+  classes.forEach(function (item) {
+    if (whatIsMyClass === item.whatIsMyClass) {
+      myDecision = item;
+    } else {
+      item + 1;
+    }
+  });
+  return myDecision;
+};
+
 //  Button and added event listener method
 document.querySelector('button').addEventListener(
   'click',
@@ -10,13 +67,6 @@ document.querySelector('button').addEventListener(
     header.textContent = 'Schedule';
     // Gets added at the end of #output
     document.querySelector('#output').appendChild(header);
-
-    // Created an array with strings to show scheudle
-    const schedule = [
-      'Monday, Biology from 11:00 am to 12:50 pm',
-      'Tuesday and Thursday, Health from 10:30 am to 11:45 am',
-      'Wednesday and Friday, no class (all day)',
-    ];
 
     // Created for each loop to show schedule
     schedule.forEach(function (schedule) {
@@ -32,56 +82,6 @@ document.querySelector('button').addEventListener(
     decision.textContent = 'Daily Decision';
     // Gets added at the end of #output
     document.querySelector('#output').appendChild(decision);
-
-    // Created an array with objects of my classes. Total of 3 objects, every single one has what my class is (whatIsMyClass), when, and time
-    const classes = [
-      {
-        whatIsMyClass: 'Biology',
-        when: 'Monday',
-        time: '11:00 am to 12:50 pm',
-      },
-      {
-        whatIsMyClass: 'Health',
-        when: 'Tuesday and Thursday',
-        time: '10:30 am to 11:45 am',
-      },
-      {
-        whatIsMyClass: 'No class',
-        when: 'Wednesday and Friday',
-        time: 'all day',
-      },
-    ];
-
-    // Created a function that tells me my class schedule and what class I have to attend
-    const whatIsMyClass = function () {
-      let classSchedule = Math.floor(Math.random() * 1100);
-      let whatIsMyClass = '';
-      if ((classSchedule = 'Monday' && classSchedule === 1100)) {
-        whatIsMyClass = 'Biology';
-        // Monday Biology class start time 11:00 am
-      } else if (
-        (classSchedule = 'Tuesday and Thursday' && classSchedule <= 1100)
-      ) {
-        whatIsMyClass = 'Health';
-        // Tuesday and Thursday Health class start time 10:30 am
-      } else {
-        whatIsMyClass = 'No class'; // Wednesday and Friday no classes
-      }
-      return whatIsMyClass;
-    };
-
-    // for each loop that tells me my decision
-    const myDecision = function (whatIsMyClass) {
-      let myDecision = '';
-      classes.forEach(function (item) {
-        if (whatIsMyClass === item.whatIsMyClass) {
-          myDecision = item;
-        } else {
-          item + 1;
-        }
-      });
-      return myDecision;
-    };
 
     // Updated daily decision using DOM and createElement method
     const dailyDecision = document.createElement('h3');
